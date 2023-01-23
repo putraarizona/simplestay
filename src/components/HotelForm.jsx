819328145015753634419b3f8b35b069c0e73b60
Putra Arizona <putraarizona8810@gmail.com>
2023-01-23 09:54:40 +0800

    Completed implementation of hotel form component, including input validation and error handling

--- src/components/HotelForm.jsx
+++ src/components/HotelForm.jsx
@@ -1,9 +1,10 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 
 import DatePicker from 'react-datepicker';
 import { ImCalendar } from 'react-icons/im';
-// import { BsPlusSquareFill } from 'react-icons/bs';
+
 import { AiFillMinusCircle, AiFillPlusCircle } from 'react-icons/ai';
+import { IoIosArrowDown } from 'react-icons/io';
 
 import './HotelForm.css';
 
@@ -12,15 +13,104 @@ import 'react-datepicker/dist/react-datepicker.css';
 const HotelForm = () => {
 	const [startDate, setStartDate] = useState(new Date());
 	const [endDate, setEndDate] = useState(new Date(new Date().setDate(new Date().getDate() + 1)));
-	const [dropdown, setDropdown] = useState(false);
+	const [rooms, setRooms] = useState(1);
+	const [guests, setGuests] = useState(1);
+
+	const [isClicked, setIsClicked] = useState(false);
+
+	const handleIncrement = (name) => {
+		if (name == 'rooms') {
+			if (rooms < 9) {
+				setRooms(parseFloat(rooms + 1));
+			} else if (rooms >= 9) {
+				setRooms(parseFloat(9));
+			}
+		}
+		if (name == 'guests') {
+			if (guests < 99) {
+				setGuests(parseFloat(guests + 1));
+			}
+		}
+	};
+
+	const handleDecrement = (name) => {
+		if (name == 'rooms') {
+			if (rooms > 1) {
+				setRooms(parseFloat(rooms - 1));
+			} else if (rooms <= 1) {
+				setRooms(parseFloat(1));
+			}
+		}
+		if (name == 'guests') {
+			if (guests > 1) {
+				setGuests(parseFloat(guests - 1));
+			}
+		}
+	};
+
+	const handleClickOutside = (event) => {
+		if (event.target.closest('.input-rooms-guests')) return;
+		setIsClicked(false);
+	};
+	useEffect(() => {
+		document.addEventListener('click', handleClickOutside, false);
+		return () => {
+			document.removeEventListener('click', handleClickOutside, false);
+		};
+	}, []);
+
+	const handleInput = (event, name) => {
+		if (name === 'rooms') {
+			if (event === '') {
+				setRooms('');
+			} else {
+				setRooms(parseFloat(event));
+			}
+		}
+		if (name === 'guests') {
+			if (event === '') {
+				setGuests('');
+			} else {
+				setGuests(parseFloat(event));
+			}
+		}
+	};
+	useEffect(() => {
+		document.addEventListener('click', handleDocumentClick);
+		return () => {
+			document.removeEventListener('click', handleDocumentClick);
+		};
+	}, [rooms, guests]);
+
+	const handleDocumentClick = (event) => {
+		if (!event.target.closest('.hide-spin-button')) {
+			if (rooms > 9) {
+				setRooms(9);
+			} else if (rooms <= 0 || isNaN(rooms)) {
+				setRooms(1);
+			} else if (!Number.isInteger(parseFloat(rooms))) {
+				setRooms(Math.floor(rooms));
+			}
+			if (guests > 99) {
+				setGuests(99);
+			} else if (guests <= 0 || isNaN(guests)) {
+				setGuests(1);
+			} else if (!Number.isInteger(parseFloat(guests))) {
+				setGuests(Math.floor(guests));
+			}
+		}
+	};
 
 	return (
 		<form action='' className='hotel-form'>
 			<div className='form-content'>
+				{/* Destination */}
 				<label className='label-destination'>Destination</label>
 				<input type='text' name='' id='' className='input-destination' placeholder='What is your destination?' />
 
+				{/* Rooms & Guests */}
 				<div className='form-row-container'>
+					{/* Checkin */}
 					<div className='form-col-container'>
 						<label>Check-In</label>
 						<div className='input-date'>
@@ -28,42 +118,78 @@ const HotelForm = () => {
 							<DatePicker selected={startDate} onChange={(date) => setStartDate(date)} dateFormat='dd/MM/yyyy' />
 						</div>
 					</div>
+
+					{/* CheckOut */}
 					<div className='form-col-container'>
 						<label>Check-Out</label>
 						<div className='input-date'>
 							<ImCalendar className='calendar-icon' />
-							<DatePicker selected={endDate} onChange={(date) => setEndDate(date)} dateFormat='dd/MM/yyyy' className='halo' />
+							<DatePicker selected={endDate} onChange={(date) => setEndDate(date)} dateFormat='dd/MM/yyyy' />
 						</div>
 					</div>
 				</div>
 
-				<div className='form-row-container'>
+				{/* Rooms & Guests */}
+				<div className='room-guest form-row-container '>
 					<div className='form-col-container'>
 						<label className='label-guests'>room & guests</label>
-						<select name='' id='' className='input-guests' onClick={() => setDropdown(!dropdown)}></select>
-						{dropdown && (
-							<div className='input-room-guest'>
-								<div className='room-container'>
+						<div type='text' className='input-rooms-guests' onClick={() => setIsClicked(!isClicked)}>
+							<p>
+								{rooms} Rooms, {guests} Guests
+							</p>
+							<IoIosArrowDown className='dropdown-icon' style={{ transform: isClicked ? 'rotate(180deg)' : 'rotate(0deg)' }} />
+						</div>
+						{isClicked && (
+							<ul onClick={(event) => event.stopPropagation()}>
+								{/* Rooms */}
+								<li>
 									<p>Rooms</p>
-									<div className='room-input-container'>
-										<AiFillMinusCircle className='minus-button button-room-guest' />
-										<input type='text' name='' id='' className='input-room' placeholder='1' />
-										<AiFillPlusCircle className='plus-button button-room-guest' />
+									<div className='room-container'>
+										<AiFillMinusCircle className='minus-button button-room-guest' onClick={() => handleDecrement('rooms')} style={{ color: rooms == 1 ? '#D9C5BD' : '#B38B7A' }} />
+										<input type='number' className='hide-spin-button' value={rooms} onChange={(event) => handleInput(event.target.value, 'rooms')} />
+										<AiFillPlusCircle className='plus-button button-room-guest' onClick={() => handleIncrement('rooms')} style={{ color: rooms == 9 ? '#D9C5BD' : '#B38B7A' }} />
 									</div>
-								</div>
-							</div>
+								</li>
+
+								{/* Guests */}
+								<li>
+									<p>Guests</p>
+									<div className='guest-container'>
+										<AiFillMinusCircle className='minus-button button-room-guest' onClick={() => handleDecrement('guests')} style={{ color: guests == 1 ? '#D9C5BD' : '#B38B7A' }} />
+										<input type='number' value={guests} className='hide-spin-button' onChange={(event) => handleInput(event.target.value, 'guests')} />
+										<AiFillPlusCircle className='plus-button button-room-guest' onClick={() => handleIncrement('guests')} style={{ color: guests == 99 ? '#D9C5BD' : '#B38B7A' }} />
+									</div>
+								</li>
+							</ul>
 						)}
 					</div>
+
+					{/* Rate Preference */}
 					<div className='form-col-container'>
 						<label className='label-rate'>rate preference</label>
 						<select type='text' name='' id='' className='input-rate'>
 							<option value='Best Available'>Best Available</option>
+							<option value='Non-Refundable' disabled>
+								Non-Refundable
+							</option>
+							<option value='Advance Purchase' disabled>
+								Advance Purchase
+							</option>
+							<option value='Corporate/Business' disabled>
+								Corporate/Business
+							</option>
+							<option value='Senior Citizen' disabled>
+								Senior Citizen
+							</option>
+							<option value='Promotional/Package' disabled>
+								Promotional/Package
+							</option>
 						</select>
 					</div>
 				</div>
 			</div>
-			{/* <label htmlFor='search' className='label-rate'></label> */}
 
+			{/* search */}
 			<input type='submit' name='search' id='search' value='search' className='search' />
 		</form>
 	);
